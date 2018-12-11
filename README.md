f2fffba6c466bd2768f156802e74ae15ee489500
Karel Molina <edmolina4@gmail.com>
2018-12-11 00:06:33 -0600

    The code was improved and the readme content modified

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 The student directory script allows you to manage the list of students enrolled al Villians Academy
 
+## How to use
+
 ```shell
 Ruby directory.rb
 ```
--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 #frist, print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -13,7 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 #finally we print the total number of students
-print "Overall, we have"
-#its important no add new line character \n
-print 11
-puts "great students"
+print "Overall, we have #{student_count} great students"
